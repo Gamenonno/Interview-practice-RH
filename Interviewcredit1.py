578299b27d459daa343516451875584d037ed45e
Gamenonno <ryanh1940@gmail.com>
2024-10-31 11:42:02 -0400

    Add files via upload

---
+++ Interviewcredit1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter interger for a: "))
+b = int(input("Enter interger for b: "))
+
+if a < b:
+    print("B is greater than A")
+    
+    
+if a > b:
+    print("A is greater than B")
+
---
+++ Interviewcredit2.py
@@ -0,0 +1,4 @@
+def printitnow():
+    print("This is a function")
+    
+printitnow()
